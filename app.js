f7fd796498f13b6d8857a4f0b7a531336fdf5d83
Sarah <49631606+gayonSarah@users.noreply.github.com>
2020-05-28 17:16:06 +1100

    eval js

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express();
 const fetch = require("node-fetch");
 var path = require("path");
 
+app.use(express.urlencoded());
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
 app.get("/products", async (req, res) => {
   const response = await fetch(
     `https://boutique-nike-nc.myshopify.com/admin/api/2020-04/products.json`,
@@ -13,9 +18,38 @@ app.get("/products", async (req, res) => {
       },
     }
   );
-
   res.send(await response.json());
 });
+
+app.get("/orders", async (req, res) => {
+  const response = await fetch(
+    `https://boutique-nike-nc.myshopify.com/admin/api/2020-04/orders.json`,
+    {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Basic M2Q2OTc2YjljNDFjN2QwZWIzZDM0NWJhOWZkNWE5MDA6c2hwcGFfNTUwZmNhYjgwYTllOGExNzMyYjYzZmUyMDQ5NzZiMjQ=`,
+      },
+    }
+  );
+  res.send(await response.json());
+});
+
+app.post("/order", async (req, res) => {
+  const response = await fetch(
+    `https://boutique-nike-nc.myshopify.com/admin/api/2020-04/orders.json`,
+    {
+      method: "post",
+      body: JSON.stringify(req.body),
+      headers: {
+        Accept: "application/json",
+        Authorization: `Basic M2Q2OTc2YjljNDFjN2QwZWIzZDM0NWJhOWZkNWE5MDA6c2hwcGFfNTUwZmNhYjgwYTllOGExNzMyYjYzZmUyMDQ5NzZiMjQ=`,
+      },
+    }
+  );
+  console.log(req.body);
+  res.send(await response.json());
+});
+
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
